dde164709c1a6d067ae44101b88fdecea18d90cc
Anvay Mayekar <74885795+anvaymayekar@users.noreply.github.com>
2020-11-29 14:14:43 +0530

    Add files via upload
    new file intrest.js

---
+++ intrest.js
@@ -0,0 +1,53 @@
+function Intrest(principal, rate, number){
+	this.currency = "₹"
+	
+	this.compoundIntrest = function(){
+		amount = (principal * Math.pow((1 + rate/100), number)).toFixed(2)
+		intrest = amount-principal
+		return `Amount: ${this.currency} ${amount}\nIntrest: ${this.currency} ${intrest}`
+	}
+	
+	this.simpleIntrest = function(){
+		intrest = (principal * rate * number)/100
+		amount = principal + intrest
+		return `Simple Intrest\nAmount: ${this.currency} ${amount}\nIntrest: ${this.currency} ${intrest}`
+	}
+
+	this.getCompoundIntrest = function(){
+		initialAmount = principal
+		for (let i = 1; i <= number; i++){
+			amount = parseFloat((principal * Math.pow((1 + rate/100), i)).toFixed(2))
+			intrest = parseFloat((amount - initialAmount).toFixed(2))
+			initialAmount = amount
+			data = {
+				year: i,
+				amount,
+				intrest
+			}
+			console.log(data)
+		}
+	}
+
+	this.getSimpleIntrest = function(){
+		initialAmount = principal
+		for (let i = 1; i <= number; i++){
+			intrest = parseFloat((principal * rate * i)/100)
+			amount = parseFloat(principal + intrest)
+			initialAmount = amount
+			data = {
+				year: i,
+				amount,
+				intrest
+			}
+			console.log(data)
+		}
+	}
+}
+
+const int = new Intrest(1000, 10, 3)
+console.log(int.simpleIntrest())
+int.getSimpleIntrest()
+console.log(int.compoundIntrest())
+int.getCompoundIntrest()
+
+module.export = Intrest
