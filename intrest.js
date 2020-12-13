08ea4d6bf5266ccc82f9082a9d6fd42eaed77a22
Anvay Mayekar <74885795+anvaymayekar@users.noreply.github.com>
2020-12-14 01:23:35 +0530

    added inflation feature

--- intrest.js
+++ intrest.js
@@ -1,24 +1,36 @@
 function Intrest(principal, rate, number){
 	this.currency = "₹"
 	
-	this.compoundIntrest = function(){
+	this.compoundIntrest = function(inflation){
 		amount = (principal * Math.pow((1 + rate/100), number)).toFixed(2)
-		intrest = amount-principal
+		intrest = (amount-principal).toFixed(2)
+		if (inflation){
+			amount = (amount * inflation).toFixed(2)
+			intrest = (intrest * inflation).toFixed(2)
+		}
 		return `Amount: ${this.currency} ${amount}\nIntrest: ${this.currency} ${intrest}`
 	}
 	
-	this.simpleIntrest = function(){
+	this.simpleIntrest = function(inflation){
 		intrest = (principal * rate * number)/100
 		amount = principal + intrest
-		return `Simple Intrest\nAmount: ${this.currency} ${amount}\nIntrest: ${this.currency} ${intrest}`
+		if (inflation){
+			amount = (amount * inflation).toFixed(2)
+			intrest = (intrest * inflation).toFixed(2)
+		}
+		return `Amount: ${this.currency} ${amount}\nIntrest: ${this.currency} ${intrest}`
 	}
 
-	this.getCompoundIntrest = function(){
+	this.getCompoundIntrest = function(inflation){
 		initialAmount = principal
 		for (let i = 1; i <= number; i++){
 			amount = parseFloat((principal * Math.pow((1 + rate/100), i)).toFixed(2))
 			intrest = parseFloat((amount - initialAmount).toFixed(2))
 			initialAmount = amount
+			if (inflation){
+				amount = parseFloat((amount * inflation).toFixed(2))
+				intrest = parseFloat((intrest * inflation).toFixed(2))
+			}
 			data = {
 				year: i,
 				amount,
@@ -28,12 +40,16 @@ function Intrest(principal, rate, number){
 		}
 	}
 
-	this.getSimpleIntrest = function(){
+	this.getSimpleIntrest = function(inflation){
 		initialAmount = principal
 		for (let i = 1; i <= number; i++){
 			intrest = parseFloat((principal * rate * i)/100)
 			amount = parseFloat(principal + intrest)
 			initialAmount = amount
+			if (inflation){
+				amount = parseFloat((amount * inflation).toFixed(2))
+				intrest = parseFloat((intrest * inflation).toFixed(2))
+			}
 			data = {
 				year: i,
 				amount,
